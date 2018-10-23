734407989b65bc0f23675095054ba2714ad3b82a
kaleb <unchartedanthem@gmail.com>
2018-10-23 10:00:53 -0600

    user created in database

--- server/controller.js
+++ server/controller.js
@@ -13,29 +13,29 @@ module.exports = {
       redirect_uri: `http://${req.headers.host}/auth/callback`
     };
 
-    // try {
-    //   let tokenRes = await axios.post(
-    //     `https://${REACT_APP_DOMAIN}/oauth/token`,
-    //     payload
-    //   );
-    //   let userRes = await axios.get(
-    //     `https://${REACT_APP_DOMAIN}/userinfo?access_token=${
-    //       tokenRes.data.access_token
-    //     }`
-    //   );
+    try {
+      let tokenRes = await axios.post(
+        `https://${REACT_APP_DOMAIN}/oauth/token`,
+        payload
+      );
+      let userRes = await axios.get(
+        `https://${REACT_APP_DOMAIN}/userinfo?access_token=${
+          tokenRes.data.access_token
+        }`
+      );
 
-    //   const { email, name, picture, sub } = userRes.data;
-    //   let foundUser = await db.find_user([sub]);
-    //   if (foundUser[0]) {
-    //     req.session.user = foundUser[0];
-    //   } else {
-    //     let createdUser = await db.create_user([name, email, picture, sub]);
-    //     // console.log("new user", createdUser)
-    //     req.session.user = createdUser[0];
-    //   }
-    // } catch (err) {
-    //   console.log(err);
-    // }
+      const { email, name, picture, sub} = userRes.data;
+      let foundUser = await db.find_user([sub]);
+      if (foundUser[0]) {
+        req.session.user = foundUser[0];
+      } else {
+        let createdUser = await db.create_user([name, email, picture, sub]);
+      
+        req.session.user = createdUser[0];
+      }
+    } catch (err) {
+      console.log(err);
+    }
     res.redirect("/#/dashboard");
   }
 };
