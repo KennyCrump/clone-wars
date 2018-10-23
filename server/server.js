06942727dbfc00791a7109a1754d4176e516ebfc
kaleb <unchartedanthem@gmail.com>
2018-10-23 09:06:10 -0600

    auth0 setup

--- server/server.js
+++ server/server.js
@@ -1,56 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
-const sessions = require('express-session')
+const session = require('express-session')
 const axios = require('axios')
-const { SERVER_PORT, REACT_APP_CLIENT_ID, REACT_APP_DOMAIN, CLIENT_SECRET, SESSION_SECRET} = process.env
 const app = express()
+const ctrl = require('./controller')
+const { SERVER_PORT, SESSION_SECRET} = process.env
 
 app.use(express.json())
 
+
+
 app.use(
-    sessions({
+    session({
       secret: SESSION_SECRET,
       resave: false,
       saveUninitialized: true
     })
   );
 
-app.get("/auth/callback", async (req, res) => {
-    let payload = {
-      client_id: REACT_APP_CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      code: req.query.code,
-      grant_type: "authorization_code",
-      redirect_uri:  `http://${req.headers.host}/auth/callback`
-    };
-    // post request with code for token
-    let tokenRes = await axios.post(
-      `https://${REACT_APP_DOMAIN}/oauth/token`,
-      payload
-    );
-    // use token to get user data
-    let userRes = await axios.get(
-      `https://${REACT_APP_DOMAIN}/userinfo?access_token=${
-        tokenRes.data.access_token
-      }`
-    );
-  
-    // const db = req.app.get("db");
-    // const { name, picture, sub } = userRes.data;
-  
-    // let foundUser = await db.find_user([sub]);
-  
-    // if (foundUser[0]) {
-    //   req.session.user = foundUser[0];
-    // } else {
-    //   let createUser = await db.create_user([name, picture, sub]);
-    //   req.session.user = createUser[0];
-    // }
-    // res.redirect("/#/dashboard");
-  })
-  
-
+  app.get('/auth/callback', ctrl.authCallback)
 
 
 app.listen(SERVER_PORT, () => {
