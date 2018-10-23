17f501dfec675d5a8fc25b06cad7a78810f34745
Kenny Crump <kennycrump@Kennys-MacBook-Pro.local>
2018-10-23 12:16:10 -0600

    Initial code-mirror-testing

--- src/Components/Challenge.js
+++ src/Components/Challenge.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import CodeMirror from 'react-codemirror'
 
 class Challenge extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            code: '// your code here'
+         }
     }
+
+    updateCode = (newCode) => {
+        this.setState({code: newCode})
+    }
+
     render() { 
+        const options = {
+            lineNumbers: true,
+            
+        }
         return (
             <h1>Challenge</h1>
+
         );
     }
 }
