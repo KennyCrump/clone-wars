00f1287efd87957c4ff0d5e19a43bcc19bb707ac
Kenny Crump <kennycrump@Kennys-MacBook-Pro.local>
2018-10-24 12:20:26 -0600

    lunch save

--- src/Components/Challenge/Challenge.js
+++ src/Components/Challenge/Challenge.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CodeMirror from 'react-codemirror';
+import axios from 'axios'
+import Nav from '../Nav/Nav'
 
 import './Challenge.css'
 require('codemirror/lib/codemirror.css');
@@ -10,13 +12,27 @@ class Challenge extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            userCode:
-`function add(num1, num2){
-    //Code Here
-}`,
-    solution: '',
-    unitTests: [{test: 'add(2, 3)', solution: 5},{test: 'add(7, 4)', solution: 11}]
+            userCode: '',
+    result: '',
+    unitTests: [{test: 'add(2, 3)', result: 5},{test: 'add(7, 4)', result: 11}]
          }
+    }
+
+    componentDidMount(){
+        axios.get(`/api/challenge/${this.props.match.params.id}`).then(res => {
+            let unitTests = []
+            for(let i = 0; i < res.data.length; i++){
+                let unitTest = {test: res.data[i].test, result: res.data[i].result}
+                unitTests.push(unitTest)
+            }
+                this.setState({
+                    userCode: res.data[0].starting_code,
+                    instructions: res.data[0].instructions,
+                    unitTests: unitTests
+                })
+            })
+            
+
     }
 
     updateCode = (newCode) => {
@@ -38,30 +54,49 @@ class Challenge extends Component {
             lineNumbers: true,
             theme: 'icecoder'
         }
-        let tests = this.state.unitTests.map(test => {
-            if(test.solution === test.userAttempt){
-                return <p className='passed'>{`${test.test} passed`}</p>
+        let tests = this.state.unitTests.map((test, index) => {
+            if(typeof(test.userAttempt) === 'number'){
+                test.result = +test.result
+            }
+            if(!test.userAttempt){
+                return <p key={index} className='failed'>{`${test.test} should return ${test.result}`}</p>
+            }
+            else if(test.result === test.userAttempt){
+                return <p key={index} className='passed'>{`${test.test} passed`}</p>
             }else{
-                return <p className='failed'>{`${test.test} should return ${test.solution} but returned ${test.userAttempt}`}</p>
+                return <p key={index} className='failed'>{`${test.test} should return ${test.result} but returned ${test.userAttempt}`}</p>
             }
         })
+        console.log(this.state.unitTests)
+        console.log(tests)
         return (
-            <div className='challengePage'>
-                <div className='instructions'><h1>Instructions</h1></div>
-                <div className='codeEditor'>
-                    <h1>Challenge</h1>
-                    <CodeMirror value={this.state.userCode} 
-                    onChange={this.updateCode} 
-                    options={options}
-                    mode='javascript'/>
-                    <button onClick={this.runCode}>Run</button>
-                    <div className='solutionWrapper'>
-                        <h1 className='textColor'>solution: </h1>
-                        <div className='solutionBox'>
-                            {this.state.unitTests[0].userAttempt ? tests : null}
-                        </div>
+            <div>
+                <Nav />
+                <div className='challengePage'>
+                    <div className='instructions'><h1>Instructions</h1>
+                        <p>
+                            {this.state.instructions}
+                        </p>
                     </div>
-                        
+                    <div className='codeEditor'>
+                        <h1>Challenge</h1>
+                        <div>
+                            {this.state.userCode &&
+                                <CodeMirror value={this.state.userCode} 
+                                onChange={this.updateCode} 
+                                options={options}
+                                mode='javascript'/>
+                            }
+                        </div>
+                        <button onClick={this.runCode}>Run</button>
+                        <div className='SolutionWrapper'>
+                            <h1 className='textColor'>Solution: </h1>
+                            <div className='solutionBox'>
+                                {tests}
+                            </div>
+                        </div>
+                            
+                    </div>
                 </div>
             </div>
 
