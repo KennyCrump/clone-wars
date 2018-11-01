3cce78c3ddc0e01674cdb6788bc48217d98745cd
Kenny Crump <kennycrump@Kennys-MacBook-Pro.local>
2018-11-01 14:21:58 -0600

    unit tests

--- src/Components/Logic/logic.js
+++ src/Components/Logic/logic.js
@@ -1,7 +1,21 @@
 const axios = require('axios');
 
-const logic = {
- 
-};
 
-module.exports = logic;
+    export function calculateScore (completed){
+        let  score = 0
+        for(let i = 0; i < completed.length; i++){
+            switch(completed[i].difficulty){
+                case 1: score += 1; break;
+                case 2: score += 3; break;
+                case 3: score += 9; break;
+                case 4: score += 20; break;
+                case 5: score += 40; break;
+                case 6: score += 80; break;
+                case 7: score += 160; break;
+                case 8: score += 320; break;
+                case 9: score += 640; break;
+                case 10: score += 1000; break;
+            }
+        }
+        return score
+    }
