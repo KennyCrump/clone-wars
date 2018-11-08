d15647e2cd21696634b216e93749298d8f8bf1e7
Kena Benioni <kenabenioni@Kenas-MacBook-Pro.local>
2018-11-08 15:18:15 -0700

    changes

---
+++ cypress/integration/about.js
@@ -0,0 +1,22 @@
+describe('can go to about page', () => {
+    it('can go to site', () => {
+        cy.visit('www.kodewars.net/#/challenges')
+    })
+    it('can click create', () => {
+        cy.viewport(1600, 660)
+        cy.get('.createbutton').click()
+    })
+    it('can select challenge name input', () => {
+        cy.viewport(1600, 660)
+        cy.get('.challengenames').click()
+    })
+    it('can type in input', () => {
+        cy.viewport(1600, 660)
+        cy.get('.challengenames').type('something here')
+    })
+    it('can go to about page', () => {
+        cy.viewport(1600, 660)
+        cy.get('[href="#/about"] > #navbuttons').click()
+    })
+
+})
